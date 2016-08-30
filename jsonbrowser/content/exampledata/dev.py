c14bf37ef65bdfd64d57862f6d50668601ada4a4
Lukas Graf <lukas.graf@4teamwork.ch>
2016-08-30 21:34:22 +0200

    Display some more details about items.

--- jsonbrowser/content/exampledata/dev.py
+++ jsonbrowser/content/exampledata/dev.py
@@ -1,9 +1,13 @@
 REPOFOLDERS = [
     dict(
+        _path=u'/ordnungssystem/fuehrung',
+        _type=u'opengever.repository.repositoryroot',
         id=u'fuehrung',
         title=u'F\xfchrung',
     ),
     dict(
+        _path=u'/ordnungssystem/allgemeines',
+        _type=u'opengever.repository.repositoryroot',
         id=u'allgemeines',
         title=u'Allgemeines',
     ),
